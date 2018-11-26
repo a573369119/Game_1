bac5f37ae8484b67ae784fafe0289d44dbc3bde9
a573369119 <573369119@qq.com>
2018-11-26 08:35:09 +0800

    commit

---
+++ game/src/Meditor/game/StartGameMeditor.ts
@@ -0,0 +1,29 @@
+/**
+ * 游戏界面 
+ */
+class StartGameMeditor extends BaseMeditor{
+    /**this.view  调用 本UI 视图*/
+
+    constructor(mediatorName?:number,view?:number,assate?:any){
+        super(mediatorName,view,assate);
+    }
+
+    /**初始化 */
+    protected init() : void
+    {
+        super.init();
+    }
+
+    /**事件绑定 */
+    protected addEvents() : void
+    {
+        
+    }
+
+    /**事件移除 */
+    protected removeEvents() : void
+    {
+
+    }
+    
+}
